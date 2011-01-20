68d264ff1150c0a3930e81178677ec7d665b775f
Steve Ivy <sivy@sixapart.com>
2011-01-19 22:48:14 -0700

    Make progress on search handler, related items templates, tests
    * Got search handler loading templates based on object type
    * Created templates dir and added initial results templates
    * Add tests dir and first test suite (00-compile wheeee)

--- plugins/RelatedItems/lib/RelatedItems/RelatedItemsField.pm
+++ plugins/RelatedItems/lib/RelatedItems/RelatedItemsField.pm
@@ -32,14 +32,110 @@ sub _field_html {
 
     my $html =
         q{<div class="textarea-wrapper"><input class="full-width" type="text" name="<mt:var name="field_name">" 
-       id="<mt:var name="field_id">"></div>};
+       id="<mt:var name="field_id">" value="<mt:Var name="field_value" escape="html">"></div>};
 
     $html .= q{<div id="results"></div>};
     return $html;
 }
 
 sub ri_list_related_items {
-    return "ri_list_related_items";
+    my $app = shift;
+    return $app->errtrans('This module works with MT::App::Search.')
+        unless $app->isa('MT::App::Search');
+
+    my ( $count, $out ) = $app->check_cache();
+    if ( defined $out ) {
+        $app->run_callbacks( 'search_cache_hit', $count, $out );
+        return $out;
+    }
+
+    if ( $app->param('count') ) {
+        $count = $app->param('count');
+        MT->log("count: $count");
+    }
+
+    return $app->errtrans("Blog_id is required.")
+        unless $app->param('blog_id');
+
+    # what kind of object are we listing?
+    return $app->errtrans("Type parameter is required.")
+        unless $app->param('type');
+
+    return $app->errtrans("Tags parameter is required.")
+        unless $app->param('tags');
+
+    $out = _render( $app, $count );
+    return unless $out;
+
+    my $result;
+    if ( ref($out) && ( $out->isa('MT::Template') ) ) {
+        defined( $result = $out->build() )
+            or return $app->error( $out->errstr );
+    }
+    else {
+        $result = $out;
+    }
+
+    $count = $out->context()->stash('number_of_events');
+
+    $app->run_callbacks( 'search_post_render', $app, $count, $result );
+
+    my $cache_driver = $app->{cache_driver};
+    $cache_driver->set( $app->{cache_keys}{count}, $count, $app->config->SearchCacheTTL );
+
+    $result;
+}
+
+sub _render {
+    my $app = shift;
+    my ($count) = @_;
+
+    my $blog_id = $app->param('blog_id');
+    $blog_id =~ s/\D//g;
+    my $blog = $app->model('blog')->load($blog_id)
+        or return $app->errtrans('Invalid blog');
+
+    my $tag_var = $app->param('tags');
+
+    if ( $tag_var =~ /,/ ) {
+        my @tags_input = split( ',', $app->param('tags') );
+        $tag_var = join( ' OR ', @tags_input );
+    }
+
+    my $plugin     = MT->component("RelatedItems");
+    my $blog_prefs = $plugin->get_config_hash("blog:$blog_id");
+
+    my $type                  = $app->param('type');
+    my $default_type_template = "ri_list_related_$type";
+
+    require MT::Template::Context;
+    my $ctx = MT::Template::Context->new;
+    $ctx->stash( 'blog',          $blog );
+    $ctx->stash( 'blog_id',       $blog_id );
+    $ctx->stash( 'local_blog_id', $blog_id );
+    $ctx->stash( 'count',         $count ) if defined $count;
+
+    my $vars = $ctx->{__stash}{vars} ||= {};
+    $vars->{this_blogid} = $blog_id;
+    $vars->{tags}        = $tag_var;
+    $vars->{count}       = $count;
+
+    my $tmpl_class = $app->model('template');
+    my $tmpl_module = $app->param('tmpl') || $default_type_template;
+
+    my $tmpl = $tmpl_class->load(
+        {
+
+            # blog_id => $blog_id,
+            identifier => $tmpl_module
+        },
+    );
+    return $app->errtrans( 'Error loading template: [_1]', $tmpl_module )
+        unless $tmpl;
+
+    $tmpl->context($ctx);
+
+    return $tmpl;
 }
 
 1;
---
+++ plugins/RelatedItems/t/00-compile.t
@@ -0,0 +1,10 @@
+use lib 't/lib', 'lib', 'extlib';
+
+use MT::Test;
+use Test::More tests => 4;
+
+ok( MT->component('relateditems'), "RelatedItems loaded" );
+
+require_ok('RelatedItems::Plugin');
+require_ok('RelatedItems::RelatedItemsField');
+require_ok('RelatedItems::Tags');
