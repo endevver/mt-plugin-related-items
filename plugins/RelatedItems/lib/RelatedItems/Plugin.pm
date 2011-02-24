e434c4368691e79deec7219b0a0ea600ee564bd8
Steve Ivy <sivy@sixapart.com>
2011-02-24 13:31:42 -0700

    add ri_field in page header, not via js

--- plugins/RelatedItems/lib/RelatedItems/Plugin.pm
+++ plugins/RelatedItems/lib/RelatedItems/Plugin.pm
@@ -19,4 +19,33 @@ sub load_customfield_types {
     };
 }
 
+sub update_template {
+    MT->log('update template');
+
+    # This is responsible for loading jQuery in the head of the site.
+    my ( $cb, $app, $template ) = @_;
+
+    # Check if jQuery has already been loaded. If it has, just skip this.
+    unless ( $$template =~ m/jquery/ ) {
+
+        # Include jQuery as part of the js_include, used on the
+        # include/header.tmpl, which is used on all pages.
+        my $old = q{<mt:setvarblock name="js_include" append="1">};
+        my $new = <<'END';
+    <script id="ri_jquery" type="text/javascript" src="<mt:StaticWebPath>jquery/jquery.js"></script>
+END
+        $$template =~ s/$old/$old$new/;
+    }
+
+    # Check if ri_field has already been loaded. If it has, just skip this.
+    unless ( $$template =~ m/ri_field/ ) {
+        my $old = q{<mt:setvarblock name="js_include" append="1">};
+        my $new = <<'END';
+    <script id="ri_field" type="text/javascript" src="<mt:PluginStaticWebPath component='relateditems'>js/ri_field.js"></script>
+END
+        $$template =~ s/$old/$old$new/;
+    }
+
+}
+
 1;
