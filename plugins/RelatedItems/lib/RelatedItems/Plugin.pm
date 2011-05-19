013b0642f7fad2d20c14f97bd6c349b8e9aed9f5
Dan Wolfgang <dan@uinnovations.com>
2011-05-19 10:52:03 -0400

    Simplify loading the JS.

--- plugins/RelatedItems/lib/RelatedItems/Plugin.pm
+++ plugins/RelatedItems/lib/RelatedItems/Plugin.pm
@@ -24,27 +24,13 @@ sub update_template {
     # This is responsible for loading jQuery in the head of the site.
     my ( $cb, $app, $template ) = @_;
 
-    # Check if jQuery has already been loaded. If it has, just skip this.
-    unless ( $$template =~ m/jquery/ ) {
-
-        # Include jQuery as part of the js_include, used on the
-        # include/header.tmpl, which is used on all pages.
-        my $old = q{<mt:setvarblock name="js_include" append="1">};
-        my $new = <<'END';
+    # Load the Javascript to make this custom field work.
+    my $old = q{<mt:setvarblock name="js_include" append="1">};
+    my $new = <<'END';
     <script id="ri_jquery" type="text/javascript" src="<mt:StaticWebPath>jquery/jquery.js"></script>
-END
-        $$template =~ s/$old/$old$new/;
-    }
-
-    # Check if ri_field has already been loaded. If it has, just skip this.
-    unless ( $$template =~ m/ri_field/ ) {
-        my $old = q{<mt:setvarblock name="js_include" append="1">};
-        my $new = <<'END';
     <script id="ri_field" type="text/javascript" src="<mt:PluginStaticWebPath component='relateditems'>js/ri_field.js"></script>
 END
         $$template =~ s/$old/$old$new/;
-    }
-
 }
 
 1;
